b6039c6ec2dc2af8305e4758debbe927e0faab89
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-24 21:11:01 -0500

    Implementación de customHooks: useFetch+useCounter

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setState({
+          loading: false,
+          error: null,
+          data,
+        });
+      });
+  }, [url]);
+
+  return state;
+};
+
+export default useFetch;
