8317103c72c8601099003b2ef09ad4215f11eceb
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-28 21:19:38 -0500

    Configuraciones básicas para la screen ToDo

---
+++ src/components/08-useReducer/TodoApp.js
@@ -0,0 +1,57 @@
+import React, { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+
+import "../../css/TodoApp.css";
+
+const TodoApp = () => {
+  const initialState = [
+    {
+      id: new Date().getTime(),
+      desc: "Aprender React",
+      done: false,
+    },
+  ];
+
+  const [todos] = useReducer(todoReducer, initialState);
+  console.log(todos);
+
+  return (
+    <div>
+      <h1>TodoApp ({todos?.length})</h1>
+      <hr />
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((item, i) => (
+              <li key={i} className="list-group-item">
+                <p className="text-center">
+                  {i + 1}. {item?.desc}
+                </p>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar to do</h4>
+          <hr />
+          <form action="">
+            <input
+              type="text"
+              name="description"
+              id=""
+              placeholder="Aprender..."
+              autoComplete="off"
+              className="form-control"
+            />
+            <button className="btn btn-outline-primary mt-1 btn-block">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoApp;
