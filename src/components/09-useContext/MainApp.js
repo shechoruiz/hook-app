4dd323611c2bc76a155f341a6ca088cc0c5fc4c9
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-03-02 19:59:40 -0500

    Implementación del createContext y useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,19 @@
 import React from "react";
 import AppRouter from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 const MainApp = () => {
-  return <AppRouter />;
+  const user = {
+    id: 1234,
+    name: "Sergio",
+    email: "ejemplo@email.com",
+  };
+
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
 
 export default MainApp;
